45d84d346ab346b0a6f71476b0b7ecdcc7a6b3ae
OlivaEduardo <lokasocba@gmail.com>
2018-03-05 21:12:22 -0300

    Código "final"

---
+++ Project1/Project1/Principal.cpp
@@ -0,0 +1,23 @@
+//Prueba de funcionamiento de Macros.
+
+#include <stdio.h>
+#include "iostream"
+
+//La siguiente macro calcula la suma de dos numeros dados.
+#define SUMA(a, b) a + b
+
+int num1;
+int num2;
+
+
+int main(void)
+{
+	printf("Ingrese un primer numero: ");
+	std::cin >> num1;
+	printf("Ingrese segundo numero: ");
+	std::cin >> num2;
+	printf("El cuadrado de la suma de los numeros ingresados es: %d \n", SUMA(num1, num2) * SUMA(num1, num2));
+	printf("Ingrese un numero para salir: ");
+	std::cin >> num1;
+	return 0;
+}
