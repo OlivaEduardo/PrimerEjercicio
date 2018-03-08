cba0a01927b56f20e8621fbaf0b603b12e7681d4
OlivaEduardo <lokasocba@gmail.com>
2018-03-08 16:04:55 -0300

    Version corregida

--- Project1/Project1/Principal.cpp
+++ Project1/Project1/Principal.cpp
@@ -3,20 +3,23 @@
 #include "iostream"
 
 //La siguiente macro calcula la suma de dos numeros dados.
-#define SUMA(a, b) a + b
-
-int num1;
-int num2;
-
+#define SUMA(a, b) ((a) + (b))
 
 int main(void)
 {
 	std::cout << "Ingrese un primer numero: ";
-	std::cin >> num1;
+	int primerNumero = 0;
+	std::cin >> primerNumero;
+
 	std::cout << "Ingrese segundo numero: ";
-	std::cin >> num2;
-	std::cout << "El cuadrado de la suma de los numeros ingresados es: " << SUMA(num1, num2) * SUMA(num1, num2) << "\n";
+	int segundoNumero = 0;
+	std::cin >> segundoNumero;
+
+	const int suma = SUMA(primerNumero, segundoNumero);
+
+	std::cout << "El cuadrado de la suma de los numeros ingresados es: " << suma * suma << "\n";
 	std::cout << "Ingrese un numero para salir: ";
-	std::cin >> num1;
+	std::cin >> primerNumero;
+
 	return 0;
 }
